df247df90359bf56292c972c0c53c5befbf5c9a5
Flynn <wphestiraid@gmail.com>
2019-06-17 22:43:36 +0900

    Handling routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 };
 
+const handleHome = (req, res) => {
+  console.log(req);
+  res.send('Hello from home');
+};
+
+const handleProfile = (req, res) => {
+  res.send('You are on my profile');
+};
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
+
 app.listen(PORT, handleListening);
