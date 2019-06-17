02bf5c8b72f92e164804ef178fcb29e6745fa423
Flynn <wphestiraid@gmail.com>
2019-06-17 22:37:03 +0900

    Your first Express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hi Express!");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
