b1bd6ea2d1c4ee9bf6078a3d1cfd8ceae3b4a6e7
Flynn <wphestiraid@gmail.com>
2019-06-19 22:06:21 +0900

    MVC Pattern part One

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ import logger from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import { userRouter } from './router';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from home');
-const handleProfile = (req, res) => res.send('You are on my profile');
 
 app.use(helmet());
 app.use(bodyParser.json());
@@ -16,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(logger('dev'));
 
-app.get('/', handleHome);
-app.get('/profile', handleProfile);
+app.use('/', globalRouter);
 app.use('/user', userRouter);
+app.use('/video', videoRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
-export const userRouter = express.Router();
+const userRouter = express.Router();
 
 userRouter.get('/', (req, res) => res.send('userIndex'));
 userRouter.get('/edit', (req, res) => res.send('userEdit'));
 userRouter.get('/password', (req, res) => res.send('userPassword'));
+
+export default userRouter;
