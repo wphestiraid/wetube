7d375a4ef6fca594232b9124eb189109e765e523
Flynn <wphestiraid@gmail.com>
2019-06-26 21:43:45 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,41 +1,14 @@
-export const videos = [
-  {
-    id: 923849,
-    title: 'Video 923849',
-    description: 'This is something I love',
-    views: 235,
-    videoFile:
-      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-    creator: {
-      id: 1,
-      name: 'Flynn',
-      email: 'wphestiraid@gmail.com'
-    }
-  },
-  {
-    id: 922992,
-    title: 'Video 922992',
-    description: 'This is something I love',
-    views: 235,
-    videoFile:
-      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-    creator: {
-      id: 1,
-      name: 'Flynn',
-      email: 'wphestiraid@gmail.com'
-    }
-  },
-  {
-    id: 922995,
-    title: 'Video 922995',
-    description: 'This is something I love',
-    views: 235,
-    videoFile:
-      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-    creator: {
-      id: 1,
-      name: 'Flynn',
-      email: 'wphestiraid@gmail.com'
-    }
-  }
-];
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/wetube', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log('Connected to DB');
+const handleError = error => console.log(`Error on DB Connection: ${error}`);
+
+db.once('open', handleOpen);
+db.on('error', handleError);
