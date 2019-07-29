bb19b9e1a7ea6fb2d8930a5bbdea67f059bfdbfb
Flynn <wphestiraid@gmail.com>
2019-07-29 22:18:27 +0900

    Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,31 @@
 const videoContainer = document.getElementById('jsVideoPlayer');
 const videoPlayer = document.querySelector('#jsVideoPlayer video');
 const playButton = document.getElementById('jsPlayButton');
+const volumeButton = document.getElementById('jsVolumeButton');
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playButton.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
+  }
+}
+
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 
 function init() {
   playButton.addEventListener('click', handlePlayClick);
+  volumeButton.addEventListener('click', handleVolumeClick);
 }
 
 if (videoContainer) {
